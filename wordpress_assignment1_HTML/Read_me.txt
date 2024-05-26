cd54ae4b0b085205497688f5c3bf5d06e13c3a38
Anjalee01 <anjaleeramwani01@gmail.com>
2024-05-26 13:49:37 +0500

    initial commit

---
+++ wordpress_assignment1_HTML/Read_me.txt
@@ -0,0 +1,5 @@
+Assignment 1: Create a portfolio page using html
+1. Add sections like about me
+2. work and experience
+3. mention your achievements in a table
+4. create a form to get feedback
---
+++ wordpress_practice/script.js
@@ -0,0 +1,51 @@
+console.log("Hello world")
+
+// // variable
+
+// // let num = 20;
+// // var  num2 = 20;
+
+// // Datatypes
+// var num1 = 30;
+// let num2 = 30.001;
+// let message = "Hello this is beatiful";
+// let message2  = true;
+
+// console.log(num1)
+
+// // Arthemetic Operators
+// // num1 = 2
+// // num2 = 20
+
+// // console.log(num1 + num2)
+
+// num1 = 2
+// num2 = 20
+
+// console.log(num1 - num2)
+
+// While Loop
+
+// ss
+
+// while(i<10) {
+//     text += "The number is" + i;
+//     i++;
+// }
+
+// while me defference ye hai ke usme condition pahele aati hai 
+
+// ARRAY
+
+// let cars = ["BMW", "MEHRAN", "Ferari"]
+// console.log(cars[1])
+// console.log(cars[-1]) //it wont work because js donot support negative indexing
+
+// console.log(cars(lengths))
+
+// OBJECTS (Dictonaries)
+
+    let person = {
+        "firstName" : "John",
+
+    }
