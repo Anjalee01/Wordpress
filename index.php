1f7fa37fa7ed98b65da6dc69dfd40b421bd85f86
Anjalee01 <anjaleeramwani01@gmail.com>
2024-10-13 23:18:10 +0500

    added category to posts

--- index.php
+++ index.php
@@ -1,31 +1,64 @@
-
 <?php get_header(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
 
+<div class="custom-header">
+
+    <img alt="" src="<?php header_image(); ?>">
+</div>
+
 <section class="content-area">
-    <div class="  left-column" >
-    <?php include('page.php'); ?>
-    <?php echo paginate_links(); ?>
-    
+    <div class="  left-column">
+        <?php
+        $query = new WP_Query( array( 'cat' => 20 ) );
+        if ($query ->have_posts()) :
+            while ($query ->have_posts()) :$query -> the_post(); ?>
+                <div class="post">
+                    <div class="post-heading">
+                        <a href="<?php the_permalink(); ?>">
+                            <h1><?php the_title(); ?></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <!-- <h1><?php the_post_thumbnail('medium'); ?></h1> -->
+                    </div>
+                    <div class="post-description">
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="signature">
+                        <b>Author: </b><?php the_author(); ?><br>
+                        <b>Time: </b><?php the_time(); ?><br>
+                        <b>Date: </b><?php the_date(); ?><br>
+                    </div>
+                </div>
+        <?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+
+        <?php echo paginate_links(); ?>
+
     </div>
-    <div class=" right-column" >
-    <!-- <h1>This  is  right area</h1>
+    <div class=" right-column">
+        <!-- <h1>This  is  right area</h1>
 
     <p>Lorem ipsum dolor sit amet.</p> -->
 
-    <?php get_sidebar(); ?>
-    
+        <?php get_sidebar(); ?>
 
 
 
@@ -37,8 +70,3 @@
 
 
 <?php get_footer(); ?>
-
-
-
-
-
