abc3a150cd2c46036d14f5daaad7b6347aa5cb0e
Anjalee01 <anjaleeramwani01@gmail.com>
2024-10-13 17:35:17 +0500

    Made Contact Us Page

---
+++ template-contact-us.php
@@ -0,0 +1,18 @@
+<?php 
+
+// This file is used for custom page theme
+// Template Name: Contact-Us Theme
+
+?>
+
+<div class="wrapper">
+<?php get_header(); ?>
+<div class="custom-header">
+<img alt="" src="<?php header_image(); ?>"> </div>
+
+<h1>Contact Us below: </h1>
+</div>
+
+
+?>
+</div>
